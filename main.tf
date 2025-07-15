a8f83b846055f4816062c6f64b95316aadf0f37f
payyaulasukanya <155073963+payyaulasukanya@users.noreply.github.com>
2025-07-15 09:58:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,7 @@ resource "null_resource" "test" {
     command = "echo Hello from GitHub"
   }
 }
+mkdir terraform-practice
+cd terraform-practice
+git init
+git remote add origin https://github.com/your-username/terraform-practice.git
